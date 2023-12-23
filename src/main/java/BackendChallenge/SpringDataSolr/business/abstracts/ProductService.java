6a5caec294a2e3d6a28a81e6de0b0617fc8f1ee8
Ercem Alkan <alkanercem@gmail.com>
2023-12-24 01:29:49 +0300

    SpringDataSolr

--- src/main/java/BackendChallenge/SpringDataSolr/business/abstracts/ProductService.java
+++
@@ -1,9 +0,0 @@
-package BackendChallenge.SpringDataSolr.business.abstracts;
-
-import java.util.List;
-
-import BackendChallenge.SpringDataSolr.entities.concretes.Product;
-
-public interface ProductService {
-	 List<Product> getProductsUpdatedAfterDate(String date);
-}
